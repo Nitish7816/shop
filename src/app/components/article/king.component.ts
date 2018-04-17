a4f7cfdd2cec21fae5d915ce338a54b66a67ece1
rahul modu <rahul.modu@acumeet.com>
2018-04-17 11:58:05 +0530

    add

--- src/app/components/article/king.component.ts
+++ src/app/components/article/king.component.ts
@@ -6,19 +6,37 @@ import {KingService} from '../../services/king.service';
   templateUrl: './king.component.html',
 })
 export class KingComponent {
-  article_id = ['select', '1001', '2002', '3003', '4004', '1003'];
+  article_id = ['select', '1578', 'H1518', 'H2405', 'H1529', '1538', 'H1530', 'H1543', 'H1540', '3171', 'H1783', '1758', '3040', '1844'
+    , 'H1731', '3167', '01914', '2572', '2004', '3153', '2525', 'H1669', '1833', 'H1844', '584', '590', '651', '510', '595', '520', '536'
+    , 'H63', '661', '703', '631', '677', '624', '711', '632', 'H601', 'H745', 'H784', '629', '712', '700', '621', 'H736', 'H712', 'H783',
+    '709', 'H740', 'H776'
+  ];
   articleMap = {
     // 'select': [''],
-    '1001': ['select', 'apple', 'orange'],
-    '2002': ['select', 'aaa', 'bbb', 'ccc'],
-    '4004': ['select', 'car', 'bike']
+     'H1518': ['select', 'Box', 'Cone 5000mtr'], 'H2405': ['select', 'Box', 'Cone 5000mtr'],
+    'H1529': ['select', 'Box', 'Cone 5000mtr'], 'H1530': ['select', 'Box', 'Cone 5000mtr'], 'H1543': ['select', 'Box', 'Cone 5000mtr'],
+    'H1540': ['select', 'Box', 'Cone 5000mtr'], 'H1783': ['select', 'Box', 'Cone 5000mtr'], 'H1731': ['select', 'Box', 'Cone 5000mtr'],
+    'H1669': ['select', 'Box', 'Cone 5000mtr'], 'H1844': ['select', 'Box', 'Cone 5000mtr'], 'H63': ['select', 'Box', 'Cone 5000mtr'],
+    'H601': ['select', 'Box', 'Cone 5000mtr'], 'H745': ['select', 'Box', 'Cone 5000mtr'], 'H784': ['select', 'Box', 'Cone 5000mtr'],
+    'H736': ['select', 'Box', 'Cone 5000mtr'], 'H712': ['select', 'Box', 'Cone 5000mtr'], 'H783': ['select', 'Box', 'Cone 5000mtr'],
+    'H740': ['select', 'Box', 'Cone 5000mtr'], 'H776': ['select', 'Box', 'Cone 5000mtr'],
+    '1538': ['select', 'Box', 'Cone 5000mtr'], '1578': ['select', 'Box', 'Cone 5000mtr'], '3171': ['select', 'Box', 'Cone 5000mtr'],
+    '1758': ['select', 'Box', 'Cone 5000mtr'], '3040': ['select', 'Box', 'Cone 5000mtr'], '2572': ['select', 'Box', 'Cone 5000mtr'],
+    '1844': ['select', 'Box', 'Cone 5000mtr'], '3167': ['select', 'Box', 'Cone 5000mtr'], '01914': ['select', 'Box', 'Cone 5000mtr'],
+    '2004': ['select', 'Box', 'Cone 5000mtr'], '3153': ['select', 'Box', 'Cone 5000mtr'], '2525': ['select', 'Box', 'Cone 5000mtr'],
+    '1833': ['select', 'Box', 'Cone 5000mtr'],
+    '584': ['select', 'Box', 'Cone 5000mtr'], '590': ['select', 'Box', 'Cone 5000mtr'], '510': ['select', 'Box', 'Cone 5000mtr'],
+    '651': ['select', 'Box', 'Cone 5000mtr'], '595': ['select', 'Box', 'Cone 5000mtr'], '520': ['select', 'Box', 'Cone 5000mtr'],
+    '536': ['select', 'Box', 'Cone 5000mtr'], '661': ['select', 'Box', 'Cone 5000mtr'], '703': ['select', 'Box', 'Cone 5000mtr'],
+    '631': ['select', 'Box', 'Cone 5000mtr'], '677': ['select', 'Box', 'Cone 5000mtr'], '624': ['select', 'Box', 'Cone 5000mtr'],
+    '711': ['select', 'Box', 'Cone 5000mtr'], '632': ['select', 'Box', 'Cone 5000mtr'], '629': ['select', 'Box', 'Cone 5000mtr'],
+    '712': ['select', 'Box', 'Cone 5000mtr'], '700': ['select', 'Box', 'Cone 5000mtr'], '621': ['select', 'Box', 'Cone 5000mtr'],
+    '709': ['select', 'Box', 'Cone 5000mtr'],
   };
   article_item= [];
   priceMap = {
-    'apple': 100,
-    'orange': 60,
-    'car': 1000000,
-    'bike': '60k'
+    'Box': 110,
+    'Cone 5000mtr': 55,
   };
   price = 0;
   aprice = 0;
